b934f2bcba8e5e16c9b3b360c04b1db91a02bde9
conceptofmind <henryshippole@gmail.com>
2026-09-22 02:09:03 +0200

    rm unused files

--- mxfp4/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-D=${DEPLOY_DIR:-/e/scratch/${PROJECT:-reformo}/$USER/k3}
-mkdir -p $D/tmp $D/cache
-export APPTAINER_CACHEDIR=$D/cache/apptainer APPTAINER_TMPDIR=$D/tmp TMPDIR=$D/tmp
-apptainer build --force --arch arm64 $D/sglang-kimi-k3-arm64.sif docker://lmsysorg/sglang:v0.5.20-cu130
--- mxfp4/up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")"
-export DEPLOY_DIR=${DEPLOY_DIR:-/e/scratch/${PROJECT:-reformo}/$USER/k3}
-[ -f $DEPLOY_DIR/sglang-kimi-k3-arm64.sif ] || bash build.sh
-bash relay.sh
-sbatch "$@" serve.sbatch
